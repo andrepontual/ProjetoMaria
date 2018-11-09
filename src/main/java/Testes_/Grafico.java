e47075e313bb9f9e10e04a9b9c69d61bddffea0b
doriziofilho <37424651+doriziofilho@users.noreply.github.com>
2018-11-09 01:09:41 -0200

    correçoes em gráficos

--- src/main/java/Testes_/Grafico.java
+++ src/main/java/Testes_/Grafico.java
@@ -1,11 +1,13 @@
 package Testes_;
 import java.awt.Color;
+import java.io.IOException;
 import java.util.List;
 
 import javax.swing.JFrame;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.plot.CategoryPlot;
@@ -15,8 +17,6 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-
-import javafx.scene.chart.CategoryAxis;
 
 public class Grafico extends JFrame{
 
@@ -56,49 +56,54 @@ public class Grafico extends JFrame{
 			DateTime dataInicio) {
 		DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
 		List<Double> previsao_1 = Lista_1;
-		System.out.println(previsao_1.size() + "   TAMANHO PREVISAO 1");
 		List<Double> previsao_2 = Lista_2;
-		System.out.println(previsao_2.size() + "   TAMANHO PREVISAO 1");
 		List<Double> previsao_3 = Lista_3;
-		System.out.println(previsao_3.size() + "   TAMANHO PREVISAO 1");
 		List<Double> previsao_4 = Lista_4;
-		System.out.println(previsao_4.size() + "   TAMANHO PREVISAO 1");
 		List<Double> previsao_5 = Lista_5;
-		System.out.println(previsao_5.size() + "   TAMANHO PREVISAO 1");
 		List<Double> previsao_6 = Lista_6;
-		System.out.println(previsao_6.size() + "   TAMANHO PREVISAO 1");
 		List<Double> previsao_7 = Lista_7;
-		System.out.println(previsao_7.size() + "   TAMANHO PREVISAO 1");
-//		DateTime dataIni = dataInicio;
-	
+		int cont = 1;
 		String datax = null; 
-		for(int i = 2; i < previsao_1.size(); i++) {
-			datax = fmt.print(dataInicio.plusMonths(i));
+		for(int i = 0; i < previsao_2.size(); i++) {
+			datax = fmt.print(dataInicio.plusMonths(cont));
 			dataSet.setValue(previsao_1.get(i), "Historico" ,String.valueOf(datax));
+			cont++;
 		}
-		for(int j = 2; j < previsao_2.size(); j++) {
-			datax = fmt.print(dataInicio.plusMonths(j));
+		cont = 1;
+		for(int j = 0; j < previsao_2.size(); j++) {
+			datax = fmt.print(dataInicio.plusMonths(cont));
 			dataSet.setValue(previsao_2.get(j), "Previsao MM - 2" ,String.valueOf(datax));
+			cont++;
 		}
-		for(int k = 4; k < previsao_3.size(); k++) {
-			datax = fmt.print(dataInicio.plusMonths(k));
+		cont = 1;
+		for(int k = 0; k < previsao_2.size(); k++) {
+			datax = fmt.print(dataInicio.plusMonths(cont));
 			dataSet.setValue(previsao_3.get(k), "Previsao MM - 4" ,String.valueOf(datax));
+			cont++;
 		}
-		for(int l = 6; l < previsao_4.size(); l++) {
-			datax = fmt.print(dataInicio.plusMonths(l));
+		cont = 1;
+		for(int l = 0; l < previsao_2.size(); l++) {
+			datax = fmt.print(dataInicio.plusMonths(cont));
 			dataSet.setValue(previsao_4.get(l), "Previsao MM - 6" ,String.valueOf(datax));
+			cont++;
 		}
-		for(int m = 2; m < previsao_5.size(); m++) {
-			datax = fmt.print(dataInicio.plusMonths(m));
+		cont = 1;
+		for(int m = 0; m < previsao_2.size(); m++) {
+			datax = fmt.print(dataInicio.plusMonths(cont));
 			dataSet.setValue(previsao_5.get(m), "Previsao SE - 2" ,String.valueOf(datax));
+			cont++;
 		}
-		for(int n = 4; n < previsao_6.size(); n++) {
-			datax = fmt.print(dataInicio.plusMonths(n));
+		cont = 1;
+		for(int n = 0; n < previsao_2.size(); n++) {
+			datax = fmt.print(dataInicio.plusMonths(cont));
 			dataSet.setValue(previsao_6.get(n), "Previsao SE - 4" ,String.valueOf(datax));
+			cont++;
 		}
-		for(int o = 6; o < previsao_7.size(); o++) {
-			datax = fmt.print(dataInicio.plusMonths(o));
+		cont = 1;
+		for(int o = 0; o < previsao_2.size(); o++) {
+			datax = fmt.print(dataInicio.plusMonths(cont));
 			dataSet.setValue(previsao_7.get(o), "Previsao SE - 6" ,String.valueOf(datax));
+			cont++;
 		}
 		
 		JFreeChart grafico = ChartFactory.createLineChart(
@@ -114,14 +119,20 @@ public class Grafico extends JFrame{
 		final CategoryPlot plot = grafico.getCategoryPlot();
 			plot.setBackgroundPaint(Color.white);
 			plot.setRenderer(0, new LineAndShapeRenderer());
+			plot.setRangeGridlinePaint(Color.BLACK);
+			plot.setDomainCrosshairColumnKey(datax);
+			
 			org.jfree.chart.axis.CategoryAxis eixo = grafico.getCategoryPlot().getDomainAxis();
 			eixo.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
-		
-			
-	        
 				
 		ChartPanel painel = new ChartPanel(grafico);
 		add(painel);
+		try {
+			ChartUtilities.saveChartAsJPEG(new java.io.File("G:/D O C U M E N T O S/D O R I Z I O/SENAI/Eclipese_N/ProjetoMaria/dist/Grafico_Sku - " + sku +".jpg"), grafico, 1200, 900);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	
